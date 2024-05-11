8a5879e969e27528586a1ffd8ab85ce6b4cbaff4
Stells228 <151114074+Stells228@users.noreply.github.com>
2024-05-11 08:13:00 +0300

    появление пёселя-шмёселя tic.js
    В прошлом случае я проводила проверку на кликабельность ячеек с появлением котика, на этот раз я хотела проверить и посмотреть работу кода при чередовании 2-ух эл-ов (картинок) и работоспособность условий

--- project/js/tic.js
+++ project/js/tic.js
@@ -1,19 +1,24 @@
 const cells = document.querySelectorAll('[data-cell]'); /*читаю все эл-ты данного класса*/
 
+let patPat = true; //слежу за текущим фото 
+
 // Обработчик клика по ячейке
 
 //перебираю каждый эл-нт в массиве
 cells.forEach(cell => {
-    cell.addEventListener('click', () => { //при тыканьи будет происхлдить магия
+    cell.addEventListener('click', () => { //при тыканьи будет происходить магия
         // Проверяю, что ячейка пуста
         if (!cell.style.backgroundImage) {
-            // Добавляю картинку в ячейку
-            cell.style.backgroundImage = 'url(/media/1.png)'; 
-            cell.style.backgroundSize = 'cover'; //вся S ячейки
-            cell.style.backgroundPosition = 'center';
+            const urlPicture = patPat ? `/media/1.png` : `/media/2.png`; //условие о том, какая картинка первее 
+
+            cell.style.backgroundImage = `url(${urlPicture})`; //вставляю значение переменной
+            cell.style.backgroundSize = `cover`;
+            cell.style.backgroundPosition = `center`;
+
+            patPat = !patPat; //смена фото для следующего клика
         }
     });
 });
 
-//я решила использовать в данном случае фоновою установку картины, чтобы
-//она не перекликалась с дальнейшими действиями в игре(та же анимация выигрыша линией)
+//я решила использовать в данном случае фоновою установку картинки, чтобы
+//она не перекликала с дальнейшими действиями в игре(та же анимация выигрыша линией)
