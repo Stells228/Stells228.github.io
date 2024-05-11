c9231554190c71b7b555c25579cf632ed664a3f3
Stells228 <151114074+Stells228@users.noreply.github.com>
2024-05-11 10:05:34 +0300

    Появление более ли менее игры tic.js
    На данном этапе получилось сделать обычную версию крестики-нолики с возможными вариациями: победил 1-ый игрок / победил 2-ой игрок / ничья, а также блокирование возможности активации(кликанья) ячеек после победы

--- project/js/tic.js
+++ project/js/tic.js
@@ -1,6 +1,7 @@
 const cells = document.querySelectorAll('[data-cell]'); /*читаю все эл-ты данного класса*/
 
 let patPat = true; //слежу за текущим фото 
+let gameOver = false; //условие конца игры
 
 // Обработчик клика по ячейке
 
@@ -8,17 +9,69 @@ let patPat = true; //слежу за текущим фото
 cells.forEach(cell => {
     cell.addEventListener('click', () => { //при тыканьи будет происходить магия
         // Проверяю, что ячейка пуста
-        if (!cell.style.backgroundImage) {
+        if (!cell.style.backgroundImage && !gameOver) {
             const urlPicture = patPat ? `/media/1.png` : `/media/2.png`; //условие о том, какая картинка первее 
 
             cell.style.backgroundImage = `url(${urlPicture})`; //вставляю значение переменной
             cell.style.backgroundSize = `cover`;
             cell.style.backgroundPosition = `center`;
 
-            patPat = !patPat; //смена фото для следующего клика
+            patPat = !patPat; //смена фото для следующего клика 
+            wonCheck(); //обробатывем возможный выигрыш
         }
     });
 });
 
 //я решила использовать в данном случае фоновою установку картинки, чтобы
 //она не перекликала с дальнейшими действиями в игре(та же анимация выигрыша линией)
+
+//победа будет при:
+const Winner = [
+    [0, 1, 2], //горизонтальные
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6], //вертикальные
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8], //диагональные
+    [2, 4, 6]
+];
+
+const messageElement = document.getElementById('message'); // Получаем элемент для вывода сообщения
+
+//проверка на наличие ничьей в целом 
+function checkDraw() {
+    let isDraw = true;
+    cells.forEach(cell => {
+        if (!cell.style.backgroundImage) {
+            isDraw = false;
+        }
+    });
+    return isDraw;
+}
+
+//проверка условий ничьей
+function checkEndGame() {
+    if (checkDraw()) {
+        messageElement.textContent = 'Ничья!';
+        gameOver = true;
+    }
+}
+//проверка победителя
+function wonCheck() {
+    for (let set of Winner) {
+        const [x, y, z] = set; //получаю индексы ячеек для проверки
+        if (cells[x].style.backgroundImage && cells[x].style.backgroundImage === cells[y].style.backgroundImage && cells[y].style.backgroundImage === cells[z].style.backgroundImage && cells[z].style.backgroundImage) {
+            const winners = cells[x].style.backgroundImage.includes('1') ? 'Мурк`аты' : 'Костом`али';
+            displayWinner(winners); //передача для объявления победителя
+            gameOver = true;
+            return;
+        }
+    }
+    //проверка на ничью
+    checkEndGame(); 
+}
+
+function displayWinner(player) {
+    messageElement.textContent = `${player} победили!`;
+}
